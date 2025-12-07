341ad5f234e34921b56d87db2b3f8eb7b02c467d
wohahef <wohafeh@gmail.com>
2025-12-07 15:01:29 +0500

    Add files via upload

---
+++ version1_input.py
@@ -0,0 +1,4 @@
+
+name = input("Enter you're name: ")
+years = int(input("Enter you're years old: "))
+print(f"hello, {name}, you're {years} years old!")
---
+++ version2_variables.py
@@ -0,0 +1,3 @@
+name = "Radmir"
+years = "14"
+print(f"Hello, {name}, you're {years} years old!")
